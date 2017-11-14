47734140b14b7c98ffb70268199881e585a96590
Alexander Solovov <a_solovov@yahoo.com>
2017-11-14 15:38:59 +0600

    Implemented multipolygon community area

--- web/src/main/webapp/js/communityArea.js
+++ web/src/main/webapp/js/communityArea.js
@@ -1,6 +1,5 @@
 function saveCommunityArea() {
     mapControl.toggleMapEditing(false);
-    var wkt = new OpenLayers.Format.WKT();
     var communityAreaLayer = mapControl.getMap().getLayer(OT.Map.LAYER_IDS.COMMUNITY_AREA);
 
     // Do Checks
@@ -9,8 +8,27 @@ function saveCommunityArea() {
         return false;
     }
 
-    var feature = communityAreaLayer.features[0].clone();
-    feature.geometry.transform(destCrs, sourceCrs);
-    $("#mainForm\\:hCommunityArea").val(wkt.write(feature));
+    var polygons = [];
+    var geom;
+
+    for (var i = 0; i < communityAreaLayer.features.length; i++) {
+        var feature = communityAreaLayer.features[i].clone();
+        geom = feature.geometry;
+        geom.transform(destCrs, sourceCrs);
+
+        if (geom.CLASS_NAME === 'OpenLayers.Geometry.MultiPolygon') {
+            for (var j = 0; j < geom.components.length; j++) {
+                polygons.push(geom.components[j]);
+            }
+        } else {
+            polygons.push(geom);
+        }
+    }
+
+    if (polygons.length > 1) {
+        geom = new OpenLayers.Geometry.MultiPolygon(polygons);
+    }
+
+    $("#mainForm\\:hCommunityArea").val(geom.toString());
     return true;
 }
